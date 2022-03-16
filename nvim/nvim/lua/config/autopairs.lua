230e4951ec6381f181fbf87f28178462e4e504c1
Thad Brown <tcb@thadbrown.com>
2022-03-16 15:58:02 +0000

    removes gunk from autopairs

--- nvim/nvim/lua/config/autopairs.lua
+++ nvim/nvim/lua/config/autopairs.lua
@@ -10,9 +10,4 @@ end
 
 vim.g.coq_settings = { keymap = { recommended = false } }
 
--- these mappings are coq recommended mappings unrelated to nvim-autopairs
-remap('i', '<esc>', [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
-remap('i', '<c-c>', [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
-remap('i', '<tab>', [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
-remap('i', '<s-tab>', [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
 return M
